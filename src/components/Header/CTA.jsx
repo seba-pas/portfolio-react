24eda1db2331cefbea9d69ee06994525d42416c4
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-08-14 03:45:11 -0300

    commiteada

---
+++ src/components/Header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className="cta">
+        <a href="" className='btn'>Download CV (spanish)</a>
+        <a href="" className='btn'>Download CV (engligh)</a>
+        <a href="#contact">Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
