33298ddd51221d946064d9d521722319a01714ab
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-08-15 12:51:10 -0300

    hey que pasa

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -6,13 +6,13 @@ import { BsInstagram } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com" target="_blank" rel='noreferrer'>
         <BsGithub />
       </a>
-      <a href="https://instagram.com" target="_blank">
+      <a href="https://instagram.com" target="_blank" rel='noreferrer'>
         <BsInstagram />
       </a>
     </div>
