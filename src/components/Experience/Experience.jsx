e455318b9315be5169d0cd2d729b4ecd8c31be44
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-09-04 20:30:03 -0300

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -43,7 +43,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Bootstrap</h4>
+                <h4>React Native</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
